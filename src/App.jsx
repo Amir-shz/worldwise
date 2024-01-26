c0ba5b5de740cf1aa86c19b995b37ed3514c79ee
amir-shz <amirsharif2079@gmail.com>
2024-01-26 22:58:34 +0330

    finish static pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import Product from "./pages/Product";
 import HomePage from "./pages/HomePage";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="Product" element={<Product />} />
-        <Route path="Pricing" element={<Pricing />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
